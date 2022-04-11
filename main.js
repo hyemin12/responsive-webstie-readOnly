32a86433d2f79639ad3420ddb0896d54ecc5bd1e
hyemin12 <khnm31@naver.com>
2022-04-11 12:35:09 +0900

    '반응형'

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const navs = document.querySelectorAll(".nav li");
 
-const one = document.getElementById("one").offsetTop;
-const two = document.getElementById("two").offsetTop;
-const three = document.getElementById("three").offsetTop;
-const four = document.getElementById("four").offsetTop;
-
 function handleNav(e) {
+  const one = document.getElementById("one").offsetTop;
+  const two = document.getElementById("two").offsetTop;
+  const three = document.getElementById("three").offsetTop;
+  const four = document.getElementById("four").offsetTop;
   navs.forEach((nav) => {
     nav.classList.remove("active");
   });
@@ -23,3 +22,17 @@ function handleNav(e) {
 navs.forEach((nav) => {
   nav.addEventListener("click", handleNav);
 });
+
+const toggle = document.querySelector("header .toggle");
+
+function handleToggle() {
+  const sidebar = document.querySelector("#side-bar");
+  if (sidebar.classList.value === "none") {
+    sidebar.classList.remove("none");
+    toggle.classList.add("open");
+  } else {
+    sidebar.classList.add("none");
+    toggle.classList.remove("open");
+  }
+}
+toggle.addEventListener("click", handleToggle);
